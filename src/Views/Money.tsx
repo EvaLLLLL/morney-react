bc5606af681ca8502649abaa34f42a3be2cd62ff
lyhhh <17610603221@163.com>
2020-09-30 20:06:50 +0800

    完成 NumberPadSection 的 UI

--- src/Views/Money.tsx
+++ src/Views/Money.tsx
@@ -69,7 +69,62 @@ const CategorySection = styled.section`
 	}
 `;
 const NumberPadSection = styled.section`
-
+	display: flex;
+	flex-direction: column;
+	> .output {
+		background: white;
+		font-size: 36px;
+		line-height: 72px;
+		text-align: right;
+		padding: 0 16px;
+		box-shadow: inset 0px -2px 3px rgba(0, 0, 0, 0.25);
+	}
+	> .pad {
+		
+		> button {
+			border: none;
+			font-size: 18px;
+			float: left;
+			height: 64px;
+			width: 25%;
+			&.ok {
+				height: 128px;
+				float: right;
+			}
+			&.zero {
+				width: 50%;
+			}
+			&:nth-child(1) {
+				background: #F2F2F2;
+			}
+			&:nth-child(2),
+			&:nth-child(5) {
+				background: #E0E0E0;
+			}
+			&:nth-child(3),
+			&:nth-child(6),
+			&:nth-child(9) {
+				background: #D3D3D3;
+			}
+			&:nth-child(4),
+			&:nth-child(7),
+			&:nth-child(10) {
+				background: #C1C1C1;
+			}
+			&:nth-child(8),
+			&:nth-child(11),
+			&:nth-child(13) {
+				background: #B8B8B8;
+			}
+			
+			&:nth-child(12) {
+				background: #9A9A9A
+			}
+			&:nth-child(14) {
+				background: #A9A9A9;
+			}
+		}
+	}
 `;
 
 const Money = () => {
@@ -97,8 +152,10 @@ const Money = () => {
 				</ul>
 			</CategorySection>
 			<NumberPadSection>
-				<div>100</div>
-				<div>
+				<div className="output">
+					100
+				</div>
+				<div className="pad clearfix">
 					<button>1</button>
 					<button>2</button>
 					<button>3</button>
@@ -110,8 +167,8 @@ const Money = () => {
 					<button>7</button>
 					<button>8</button>
 					<button>9</button>
-					<button>OK</button>
-					<button>0</button>
+					<button className="ok">OK</button>
+					<button className="zero">0</button>
 					<button>.</button>
 				</div>
 			</NumberPadSection>
