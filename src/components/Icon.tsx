7337829aa3764d731c69841e3e1c31c85b957229
lyhhh <17610603221@163.com>
2020-09-30 17:52:35 +0800

    修复 Icon Nav 点击区域过小的问题

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
-
 type Props = {
 	name: string
 }
