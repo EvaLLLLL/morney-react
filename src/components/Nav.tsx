f1cdbdcc04e244783206838b2c6c26027d018687
lyhhh <17610603221@163.com>
2020-09-30 18:09:23 +0800

    完成导航点击高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -22,6 +22,12 @@ const NavWrapper = styled.div`
 					width: 24px;
 					height: 24px;
 					}
+					&.selected {
+						color: #6566ff;
+						.icon {
+							fill: #6566ff;
+						}
+					}
 				}
 			}
 		}
@@ -32,21 +38,21 @@ const Nav = () => {
 		<NavWrapper>
 			<ul>
 				<li>
-					<Link to="/tags">
+					<NavLink to="/tags" activeClassName="selected">
 						<Icon name="label"/>
 						标签页
-					</Link>
+					</NavLink>
 				</li>
 				<li>
-					<Link to="/money">
+					<NavLink to="/money" activeClassName="selected">
 						<Icon name="money"/>
 						记账页
-					</Link>
+					</NavLink>
 				</li>
 				<li>
-					<Link to="/statistics">
+					<NavLink to="/statistics" activeClassName="selected">
 						<Icon name="chart"/>统计页
-					</Link>
+					</NavLink>
 				</li>
 			</ul>
 		</NavWrapper>
