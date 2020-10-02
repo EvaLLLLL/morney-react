b1dca2b71821186131a69c42cfbcc2bf59203898
lyhhh <17610603221@163.com>
2020-10-02 23:42:48 +0800

    基本完成 NumberPad 功能

--- src/Views/Money/NumberPadSection.tsx
+++ src/Views/Money/NumberPadSection.tsx
@@ -60,7 +60,15 @@ const NumberPadWrapper = styled.section`
 `;
 
 const NumberPadSection: React.FC = () => {
-	const [output, setOutput] = useState('0');
+	const [output, _setOutput] = useState('0');
+	const setOutput = (output: string) => {
+		if (output.length > 16) {
+			output = output.slice(0, 16);
+		} else if (output.length === 0) {
+			output = '0';
+		}
+		_setOutput(output);
+	};
 	const onClickButtonWrapper = (e: React.MouseEvent) => {
 		const text = (e.target as HTMLButtonElement).textContent;
 		if (text === '') {
@@ -77,21 +85,30 @@ const NumberPadSection: React.FC = () => {
 			case '7':
 			case '8':
 			case '9':
-			case '.':
 				if (output === '0') {
 					setOutput(text);
 				} else {
 					setOutput(output + text);
 				}
 				break;
+			case '.':
+				if (output.indexOf('.') >= 0) {
+					return;
+				}
+				setOutput(output + '.');
+				break;
 			case '删除':
-				console.log('删除');
+				if (output.length === 1) {
+					setOutput('0');
+				} else {
+					setOutput(output.slice(0, -1));
+				}
 				break;
 			case '清空':
-				console.log('清空');
+				setOutput('');
 				break;
 			case 'OK':
-				console.log('OK');
+				console.log('确认');
 				break;
 		}
 	};
